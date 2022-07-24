e25267f4fcda27ddcbe0b9e8b871b798c7766877
paakowden <paakow1114@gmail.com>
2022-07-24 13:14:19 +0000

    Nextjs-reddit-clone

---
+++ graphql/queries.ts
@@ -0,0 +1,110 @@
+import { gql } from "@apollo/client";
+
+export const GET_POST_BY_POST_ID = gql`
+  query Myquery($post_id: ID!) {
+    getPostListByPostId(post_id: $post_id) {
+      body
+      comments {
+        created_at
+        id
+        post_id
+        text
+        username
+      }
+      created_at
+      id
+      image
+      subreddit {
+        created_at
+        id
+        topic
+      }
+      title
+      subreddit_id
+      username
+      votes {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
+    }
+  }
+`;
+
+export const GET_ALL_POSTS = gql`
+  query Myquery {
+    getPostList {
+      body
+      created_at
+      id
+      image
+      title
+      subreddit_id
+      username
+      comments {
+        created_at
+        id
+        post_id
+        text
+        username
+      }
+      subreddit {
+        created_at
+        id
+        topic
+      }
+      votes {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
+    }
+  }
+`;
+
+export const GET_ALL_POSTS_BY_TOPIC = gql`
+  query Myquery($topic: String!) {
+    getPostListByTopic(topic: $topic) {
+      body
+      created_at
+      id
+      image
+      title
+      subreddit_id
+      username
+      comments {
+        created_at
+        id
+        post_id
+        text
+        username
+      }
+      subreddit {
+        created_at
+        id
+        topic
+      }
+      votes {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
+    }
+  }
+`;
+
+export const GET_SUBREDDIT_BY_TOPIC = gql`
+  query Myquery($topic: String!) {
+    getSubredditListByTopic(topic: $topic) {
+      id
+      topic
+      created_at
+    }
+  }
+`;
---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Feed from "../components/Feed";
+import PostBox from "../components/PostBox";
+
+const Home: NextPage = () => {
+  return (
+    <div className="my-7 mx-auto max-w-5xl">
+      <Head>
+        <title>Reddit 2.0 Clone</title>
+      </Head>
+
+      {/* Post Box */}
+      <PostBox />
+
+      <div className="flex">
+        <Feed />
+      </div>
+      <div className="flex">{/* Feed */}</div>
+    </div>
+  );
+};
+
+export default Home;
